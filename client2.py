8e3b1ce7b4b5c093d64d3d245d037b84f049e447
Rob Lewis <rlewis1@andrew.cmu.edu>
2017-09-12 13:09:54 -0400

    hide new declarations in namespaces

--- client2.py
+++ client2.py
@@ -1,44 +1,41 @@
 import socket
 import sys
 import os
+import argparse
 
-def process(s):
+def process(s, is_global):
     sep = ' '
     clientsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     clientsocket.connect(('localhost', 10000))
-    clientsocket.send(s)
+    clientsocket.send(s + ("1" if is_global else "0"))
     buf = ''
     while sep not in buf:
         buf += clientsocket.recv(8)
-    #print "buf is:", buf
     splt = buf.split(sep, 1)
     num = int(splt[0])
-    #print "num = ", num
     recvd = 8
     buf = splt[1]
     while recvd < num:
         buf += clientsocket.recv(8)
         recvd += 8
     buf += clientsocket.recv(8)
-    #buf = clientsocket.recv(1000)
-    #f = open("/e/Dropbox/lean/mathematica/temp","w")
-    #f.write("writing...\n")
-    #f.write(str(num) + "\n")
-    #f.write(buf)
-    #f.write("\n\n" + str(len(buf)))
-    #f.close()
     print buf
 
-def read_from_file():
-    path = sys.argv[2]
+def read_from_file(path):
     f = open(path, "r")
     s = f.read()
     f.close()
     os.remove(path)
     return s
 
-if sys.argv[1] == "-f":
-    process(read_from_file())
+parser = argparse.ArgumentParser(description="Communicate with Mathematica.")
+parser.add_argument('-f', action='store_true') # file 
+parser.add_argument('-g', action='store_true') # global
+parser.add_argument('cmd')
+args = parser.parse_args()
+
+if args.f:
+    process(read_from_file(args.cmd), args.g)
 else:
-    process(sys.argv[1])
+    process(args.cmd, args.g)
 
--- server2.m
+++ server2.m
@@ -1,3 +1,5 @@
+(* ::Package:: *)
+
 << "SocketLink`"
 << "lean_form.m"
 
@@ -9,22 +11,41 @@ ToWindowsDir[s_String] :=
 DirectoryFormat[s_String] := 
  If[WindowsDirQ[Directory[]], ToWindowsDir[s], s]
 
+
+$ContextPath = Append[$ContextPath,"MyGlobalContext`"];
+$Context = "MyGlobalContext`";
+cctr = 0;
+mgc = "MyGlobalContext`";
+ClearAll["Global`*"];
+
 CreateAsynchronousServer[CreateServerSocket[10000], Handler]
 
 TestNextChar[in_] := BinaryRead[in, "Character8"] != "&"
+WriteLine["stdout",""];
 
-Handler[{in_InputStream, out_OutputStream}] := 
-  Module[{o = "", r = "", c = ""},(*While[True,TimeConstrained[r=r<>
-   BinaryRead[in,"Character8"],0.05,(Close[in];Break[])]];*)
-   While[True, c = BinaryRead[in, "Character8"]; 
-    If[c == "&" && TestNextChar[in], (Close[in]; Break[]), 
-     r = r <> c]];
-   WriteString["stdout", "Received:\n" <> r <> "\n\nOutput:"];
-   o = ToExpression[r] // OutputFormat;
-   WriteString[out, ToString[StringLength[o]] <> " "];
-   WriteString[out, o];
-   WriteString["stdout", o <>"\n\n"];
-   Close[out];];
+
+Handler[{in_InputStream, out_OutputStream}] :=
+  Module[{o = "", r = "", c = "", g = 0, xct},
+         While[True, c = BinaryRead[in, "Character8"];
+         If[c == "&" && TestNextChar[in],
+            (g = BinaryRead[in, "Character8"] // ToExpression; Close[in]; Break[]), 
+             r = r <> c]];
+         WriteLine["stdout", "Received:"];
+         WriteLine["stdout",  r <> "\n"];
+         WriteLine["stdout", "Global: " <> ToString[g]];
+         WriteLine["stdout", "Output:"];
+         xct = If[g==0,"LeanLinkCtx`",mgc];
+         (*{begin, end} = If[g==0,{"LeanLinkCtx`", "$Context=mgc;cctr=cctr+1;"},{mgc, "Null;"}];*)
+         $Context = xct;
+         o = ToExpression[r] // OutputFormat;
+         WriteString[out, ToString[StringLength[o]] <> " "];
+         WriteString[out, o];
+         $Context = mgc;
+         ClearAll["Global`*"];
+         ClearAll["LeanLinkCtx`*"];
+         WriteString["stdout", o <>"\n\n"];
+         Close[out];
+  ]
 
 OutputFormat[i_Integer] := "I[" <> ToString[i] <> "]"
 OutputFormat[s_String] := "T[\"" <> s <> "\"]"
